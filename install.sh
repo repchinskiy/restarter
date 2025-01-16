033de6e9441f8b725ed4f0fa82d9b21f10e14283
repchinskiy <repchinskiy@gmail.com>
2025-01-16 22:18:24 +0200

    Add files via upload

---
+++ install.sh
@@ -0,0 +1,83 @@
+#!/bin/bash
+
+TG_URL=$1
+TG_CHAT_ID=$2
+CMD_URL=$3
+
+function src_git_repo {
+  #check if directory exists
+  if [ -d "/root/root/restarter" ]; then
+    echo "restarter exists, pulling latest changes"
+    cd /root/restarter/restarter || exit
+    sudo -u root git pull
+  else
+    echo "restarter does not exist, cloning repo"
+    sudo -u root mkdir -p /root/restarter/restarter
+    sudo -u root git clone https://github.com/repchinskiy/restarter /root/restarter/restarter
+    cd /root/restarter/restarter || exit
+  fi
+}
+
+function prepare_python_env {
+  if [ -d "/root/restarter/venv" ]; then
+    echo "venv exists, it's ok"
+    sudo -u root bash << EOF
+    source /root/checker/venv/bin/activate
+    source /root/checker_tg/venv/bin/activate
+    pip install -r /root/restarter/restarter/requirements.txt
+EOF
+    return
+  else
+    cd /root/restarter || exit
+    
+    # Проверяем наличие виртуального окружения
+    if [ ! -d "venv" ]; then
+        echo "venv does not exist, installing requirements"
+        
+        # Устанавливаем python3-venv
+        sudo apt-get install python3-venv -y
+        
+        # Создаем виртуальное окружение от пользователя root
+        sudo -u root python3 -m venv venv
+    fi
+    
+    # Активируем виртуальное окружение и устанавливаем зависимости от пользователя root
+    sudo -u root bash << EOF
+    source /root/checker/venv/bin/activate
+    source /root/checker_tg/venv/bin/activate
+    pip install -r /root/restarter/restarter/requirements.txt
+EOF
+  fi
+
+}
+
+function create_systemd {
+  sudo tee <<EOF >/dev/null /etc/systemd/system/restarter.service
+[Unit]
+Description=Restarter
+After=network-online.target
+StartLimitIntervalSec=0
+[Service]
+User=root
+Restart=always
+RestartSec=3
+LimitNOFILE=65535
+#todo тут на основных аках не прокатит
+ExecStart=/root/checker_tg/venv/bin/python3 /root/restarter/restarter/restarter.py $TG_URL $TG_CHAT_ID $CMD_URL
+#ExecStart=/root/.pyenv/versions/3.11.10/bin/python3.11 /root/restarter/restarter/restarter.py $TG_URL $TG_CHAT_ID $CMD_URL
+[Install]
+WantedBy=multi-user.target
+EOF
+
+  sudo systemctl daemon-reload
+  sudo systemctl enable checker_tg
+  sudo systemctl restart checker_tg
+}
+
+function main {
+  src_git_repo
+  prepare_python_env
+  create_systemd
+}
+
+main
---
+++ restarter.py
@@ -0,0 +1,111 @@
+import datetime
+import socket
+import subprocess
+import sys
+import threading
+import time
+
+import requests
+from flask import Flask
+
+app = Flask(__name__)
+
+TELEGRAM_ALERT_BASE_URL = ""
+chat_id = ""
+check_commands_url = []
+
+def get_ip_address():
+    s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    s.connect(("8.8.8.8", 80))
+    return s.getsockname()[0]
+
+
+def init():
+    writeln("Init ...\n")
+
+    global TELEGRAM_ALERT_BASE_URL, chat_id
+
+    # if len(sys.argv) > 1:
+    #     need_restart_node = int(sys.argv[1]) == 1
+
+    cmd_url = "unknown"
+    if len(sys.argv) > 1:
+        TELEGRAM_ALERT_BASE_URL = "https://api.telegram.org/" + sys.argv[1] + "/sendMessage"
+    if len(sys.argv) > 2:
+        chat_id = sys.argv[2]
+    if len(sys.argv) > 3:
+        cmd_url = sys.argv[3]
+        check_commands_url.append(cmd_url)
+
+    println(
+        f'sys_params: {sys.argv[0]} TELEGRAM_ALERT_BASE_URL: {TELEGRAM_ALERT_BASE_URL} chat_id: {chat_id} cmd_url: {cmd_url}')
+
+    run_updater_background()
+
+
+def fetch_data(url):
+    response = requests.get(url)
+    response.raise_for_status()  # Проверка на ошибки HTTP
+    return response.text
+
+
+def parse_data(raw_data):
+    lines = raw_data.strip().split("\n")
+    return [line.split(";") for line in lines]
+
+def writeln(msg):
+    sys.stdout.write(msg + "\n")
+
+
+def println(msg):
+    sys.stdout.write(str(datetime.datetime.now()) + ": " + msg + "\n")
+
+def process():
+    for commandUrl in check_commands_url:
+        println(f'command_url: {commandUrl}')
+        raw_data = fetch_data(commandUrl)
+        nodes_data = parse_data(raw_data)
+        for cmd_name, cmd_check, cmd_restart in nodes_data:
+            println(f'cmd_name: {cmd_name} cmd_check:{cmd_check} cmd_restart: {cmd_restart}')
+            if cmd_name.startswith("#"): continue
+            result = subprocess.run(cmd_check, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+            status = result.returncode
+            if not status:
+                subprocess.run(cmd_restart, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+                content = f'{get_ip_address()}\n{cmd_name}'
+                println(f'tg_url: {TELEGRAM_ALERT_BASE_URL} chat_id: {chat_id} text: {content}')
+                requests.get(url=TELEGRAM_ALERT_BASE_URL, params={'chat_id': chat_id, 'text': content})
+
+    # f = open("/root/checker_tg/checker_tg/check_commands_local.csv", "r")
+    # lines = f.readlines()
+    # f.close()
+    # nodes_data = [line.split(";") for line in lines]
+    #
+    # for node_name, command in nodes_data:
+    #     println(f'node_name: {node_name} command:{command}')
+    #     if node_name.startswith("#"): continue
+    #     status = check_node(command, ip_addr)
+    #     status_metric = "active" if status else "INACTIVE"
+    #     metrics_output.append(f'{node_name}: {status_metric}')
+
+def run_updater():
+    while True:
+        process()
+        time.sleep(60 * 10)
+
+
+def run_updater_background():
+    writeln("Run background tasks ...\n")
+    updater_thread = threading.Thread(target=run_updater)
+    updater_thread.start()
+
+
+def get_sys_info():
+    return get_ip_address()
+
+def test():
+    writeln("Test mode: ")
+
+if __name__ == "__main__":
+    # test()
+    init()
